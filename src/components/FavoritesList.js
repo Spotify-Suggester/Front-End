090e917e3bf9defd17961dfd07157540f7a21732
aland1013 <adam.land@gmail.com>
2020-06-18 23:32:37 -0400

    created static search results and built out FavoritesList functionality

--- src/components/FavoritesList.js
+++ src/components/FavoritesList.js
@@ -1,8 +1,32 @@
-// will iterate through array of user's favorite songs and renders each Favorite
-import React from 'react';
+// will iterate through array of user's favorite songs and render each Favorite
+import React, { useContext } from 'react';
+import { FavoritesContext } from '../context/FavoritesContext';
 
 const FavoritesList = () => {
-  return <h3>FavoritesList</h3>;
+  const { favorites, setFavorites } = useContext(FavoritesContext);
+  return favorites.length === 0 ? (
+    <h3>Choose some favorites</h3>
+  ) : (
+    <>
+      <h3>FavoritesList</h3>
+      {favorites.map((favorite) => {
+        return (
+          <div key={favorite.id}>
+            <h4>{favorite.name}</h4>
+            <p>Artist: {favorite.artists[0].name}</p>
+            <p>Album: {favorite.album.name}</p>
+            <button
+              onClick={() =>
+                setFavorites(favorites.filter((item) => item.id != favorite.id))
+              }
+            >
+              remove
+            </button>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default FavoritesList;
--- src/components/FavoritesPage.js
+++ src/components/FavoritesPage.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FavoritesContext } from '../context/FavoritesContext';
 import FavoritesList from './FavoritesList';
 import FavoritesForm from './FavoritesForm';
 import SuggestionForm from './SuggestionForm';
+import SearchResults from './SearchResults';
+
+import { data } from '../data';
 
 const FavoritesPage = () => {
+  const [favorites, setFavorites] = useState([]);
+  const [results, setResults] = useState(data);
+
   return (
-    <FavoritesContext.Provider>
+    <FavoritesContext.Provider value={{ favorites, setFavorites, results }}>
       <FavoritesList />
       <FavoritesForm />
       <SuggestionForm />
+      <SearchResults />
     </FavoritesContext.Provider>
   );
 };
