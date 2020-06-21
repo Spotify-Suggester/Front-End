0d0e82c5a1ca94615f32c59d25bd592709c91611
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-06-21 16:01:12 -0400

    changed styles 2 work with MaterialUI makeStyle()

--- src/components/CustomCheckbox.js
+++ src/components/CustomCheckbox.js
@@ -1,16 +1,44 @@
 import React from 'react';
-
+import Button from '@material-ui/core/Button';
+import {makeStyles} from "@material-ui/core/styles";
 
 
 
 function CustomCheckbox(props) {
 
-  const buttonStyle = {
-    background: `linear-gradient(rgba(255,101,132,.3), rgba(255,101,132,.3)), url(${props.imageURL}) center`,
-    backgroundSize: `cover`,
-  }
+  const useStyle = makeStyles(() => ({
+    button: {
+      
+      fontSize: "4rem",
+      width: "22%",
+      height: "130px",
+      margin: "5px 0 15px 0",
+      color: "#111",
+      border: "2px solid #FF6584",
+      background: `linear-gradient(rgba(255,101,132,.5), rgba(255,101,132,.5)), url(${props.imageURL}) center`,
+      backgroundSize: `cover`,
+      fontSize: "1.2rem",
+      fontWeight: "600",
+      "& .MuiButton-label": {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        "& i": {
+          fontSize: "4.5rem"
+        }
+      }
+    },
+    span: {
+      fontSize: "1.2rem",
+      fontWeight: "600",
+      
+    }
+  }));
+
+  const classes = useStyle();
   return (
-    <button style={buttonStyle}><i className={props.fontAwesomeIcon}></i><span>Rock</span></button>
+    <Button className={classes.button}><i className={props.fontAwesomeIcon}></i><span className={classes.span}>Rock</span></Button>
   );
 }
 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,22 +2,86 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import {Container, Grid, Box} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles(() => ({
+  container: {
+    margin: '0',
+    padding: '0'
+  },
+  formContainer: {
+    padding: "20px 0 30px"
+  },
+  form: {
+    display: "flex",
+    position: "relative",
+    width: "100%",
+    color: "white",
+    "& .MuiFormControl-fullWidth": {
+      marginRight: "10px"
+    },
+    "& label, & input, & .MuiButton-contained": {
+      color: "white",
+    },
+
+    "& .MuiButton-contained": {
+      backgroundColor: "#6C63FF",
+    },
+
+    "& .MuiInput-underline:before": {
+      borderBottomColor: "rgba(255, 255, 255, 0.7) !important;",
+    },
+
+    "& .MuiFormLabel-root.Mui-focused": {
+      color: "#6C63FF !important",
+    },
+
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "#6C63FF !important",
+    },
+
+    "& .MuiButton-contained.Mui-disabled": {
+      color: "rgba(0, 0, 0, .45)",
+    },
+
+    "& .MuiDivider-root": {
+      backgroundColor: "rgba(255,255,255,0.7)",
+    },
+  },
+}));
 
 const SearchForm = () => {
+  const classes = useStyles();
   return (
-    <>
-      <form>
-        <TextField
-          name='search-bar'
-          id='search-bar'
-          label='Search your favorites songs'
-          fullWidth
-        />
-        <Button variant='contained' color='secondary'>
-          Search
-        </Button>
-      </form>
-    </>
+    <Container maxWidth={false} className={classes.container}>
+      <Grid item xs={12} md={12} className={classes.formContainer}>
+        <Box className={classes.form}>
+          <TextField
+            name='search-bar'
+            id='search-bar'
+            label='Search your favorites songs'
+            fullWidth
+          />
+          <Button variant='contained' color='secondary'>
+            Search
+          </Button>
+        </Box>
+      </Grid>
+    </Container>
+
+
+
+
+    // <>
+    //   <form>
+    //     
+    //     <Button variant='contained' color='secondary'>
+    //       Search
+    //     </Button>
+    //   </form>
+    // </>
   );
 };
 
