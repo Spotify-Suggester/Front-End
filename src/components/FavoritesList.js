ec9f0d9d27f9f3ff72db84d0df9e998de2b9a7b4
aland1013 <adam.land@gmail.com>
2020-06-25 15:27:40 -0600

    changed api endpoint to get suggested songs

--- src/components/FavoritesList.js
+++ src/components/FavoritesList.js
@@ -1,6 +1,7 @@
 // will iterate through array of user's favorite songs and render each Favorite
 import React, { useContext } from 'react';
 import { FavoritesContext } from '../contexts/FavoritesContext';
+import { UserContext } from '../contexts/UserContext';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 import ListComponent from './ListComponent';
@@ -10,40 +11,41 @@ import { axiosWithUserAuth } from '../utils/axiosWithAuth';
 
 const useStyles = makeStyles(() => ({
   container: {
-    width: "450px",
-    margin: "0",
-    borderRight: "1px solid #6c63FF",
-    background: "rgba(0,0,0,.45)",
-    position: "fixed",
-    zIndex: "100",
-    height: "100vh",
-    "& .MuiButton-contained": {
-      backgroundColor: "#6C63FF",
-      color: "white",
-      width: "calc(100% - 48px)",
-      position: "absolute",
-      bottom: "100px",
-      "&:hover": {
-        background: "#4a41d4"
+    width: '450px',
+    margin: '0',
+    borderRight: '1px solid #6c63FF',
+    background: 'rgba(0,0,0,.45)',
+    position: 'fixed',
+    zIndex: '100',
+    height: '100vh',
+    '& .MuiButton-contained': {
+      backgroundColor: '#6C63FF',
+      color: 'white',
+      width: 'calc(100% - 48px)',
+      position: 'absolute',
+      bottom: '100px',
+      '&:hover': {
+        background: '#4a41d4'
       }
-    },
+    }
   },
   header: {
     fontWeight: 300,
-    textAlign: "center"
+    textAlign: 'center'
   }
-}))
+}));
 const FavoritesList = (props) => {
   const classes = useStyles();
   const { favorites, setFavorites, setSuggestions } = useContext(
     FavoritesContext
   );
+  const { userId } = useContext(UserContext);
 
   const getSuggestions = () => {
-    let seeds = [];
-    favorites.forEach((favorite) => seeds.push(favorite.id));
     axiosWithUserAuth()
-      .get('https://spotify-suggester1.herokuapp.com/api/users/1/recommend')
+      .get(
+        `https://spotify-suggester1.herokuapp.com/api/users/${userId}/recommend`
+      )
       .then((res) => {
         console.log('get res', res);
         setSuggestions(res.data.recommended_songs);
